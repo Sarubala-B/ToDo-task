f7f138845cc1c72bef7cb4004f551a2c6d8ad6f2
Sarubala-B <poojasaru1112000@gmail.com>
2024-11-07 18:18:43 +0530

    update

---
+++ config/database.js
@@ -0,0 +1,8 @@
+const  Sequelize  = require('sequelize');
+
+const sequelize = new Sequelize('todo_db', 'root', 'Sarubala', {
+    host: 'localhost',
+    dialect: 'mariadb'
+});
+
+module.exports =  sequelize;
